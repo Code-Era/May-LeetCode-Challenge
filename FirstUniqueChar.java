4c43ae04550f2cad7139c4de32f459b2110b59b5
Amit Kumar Singh <64722139+Code-Era@users.noreply.github.com>
2020-05-05 21:52:09 +0530

    day -5

---
+++ FirstUniqueChar.java
@@ -0,0 +1,16 @@
+class FirstUniqueChar {
+    public int firstUniqChar(String s) {
+       
+        var map = new LinkedHashMap<Character, Integer>();
+        
+        for (Character c : s.toCharArray()){
+             map.put(c , map.containsKey(c) ? map.get(c)+1 : 1);
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+             if (map.get(s.charAt(i)) == 1) 
+                return i;
+            }
+        return -1;
+   }
+}
