8137820703f1e455370f0418a5ea2e2f083e4fef
Amit Kumar Singh <64722139+Code-Era@users.noreply.github.com>
2020-05-05 21:49:35 +0530

    Q -1

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -1,14 +1,14 @@
 public class FirstBadVersion extends VersionControl {
     public int firstBadVersion(int n) {
         int low = 1;
-    int right = n;
-    while (low < right) {
-        int mid = low + (right - low) / 2;
-        if (isBadVersion(mid)) {
-            right = mid;
-        } else {
-            low = mid + 1;
-        }
+        int right = n;
+         while (low < right) {
+            int mid = low + (right - low) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                low = mid + 1;
+          }
     }
     return low;
     }
