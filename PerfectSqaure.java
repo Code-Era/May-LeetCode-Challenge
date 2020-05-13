ffe961187a00de767a0d291b5050484949165e86
Amit Kumar Singh <64722139+Code-Era@users.noreply.github.com>
2020-05-13 23:09:56 +0530

    "solution without in-built function"

--- PerfectSqaure.java
+++ PerfectSqaure.java
@@ -4,4 +4,19 @@ class PerfectSqaure {
         
         return (value - Math.floor(value)  == 0);
     }
+    // add normal method
+    public static boolean isSquare(int n){
+        if(n==0 || n==1)
+            return true;
+        for (int i = 0; i <n/2 ; i++) {
+            int x = i*i;
+            if(x==n)
+                return true;
+            else if (n<x)
+                return false;
+            else
+                continue;
+        }
+        return false;
+    }
 }
